7feab38900e0813c280230473d9efd254d3695d5
Evan Leister <evanl@princeton.edu>
2012-10-20 14:35:25 -0400

    Fixed testapproxJacobian1, corrected derivative defn, moved v definitions

--- functions.py
+++ functions.py
@@ -8,10 +8,16 @@ def ApproximateJacobian(f, x, dx=1e-6):
         n = 1
     fx = f(x)
     Df_x = N.matrix(N.zeros((n,n)))
+
+    v = N.matrix(N.zeros((n,1)))
+    v[:,0] = dx
+
+    print "n", n
+    print "v", v 
+    print "fx" ,fx
+
     for i in range(n):
-        v = N.matrix(N.zeros((n,1)))
-        v[i,0] = dx
-        Df_x[:,i] = f(x + v) - fx
+        Df_x[:,i] = (f(x + v) - fx) / v
     return Df_x
 
 class Polynomial(object):
