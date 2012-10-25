887ffd2336df8d8c6072106b6cb27f5b1fa7c240
Evan Leister <evanl@princeton.edu>
2012-10-25 16:50:32 -0400

    Added testSingleLinearStep, updated .gitignore to ignore .swp, added some boilerplate to readme

--- readme.md
+++ readme.md
@@ -1,1 +1,13 @@
 APC 524 Homework 3 
+
+This homework submission contains the following Python script files:
+
+  - testFunctions.py
+  - testNewton.py 
+
+which depend on the following class and function definition files:
+
+  - functions.py
+  - newton.py
+
+
--- testNewton.py
+++ testNewton.py
@@ -11,5 +11,12 @@ class TestNewton(unittest.TestCase):
         x = solver.solve(2.0)
         self.assertEqual(x, -2.0)
 
+    def testSingleLinearStep(self):
+        def f(x):
+          return 4.0 * x + N.exp(x)
+        solver = newton.Newton(f, tol = 1.e-15, maxiter=1, dx = 1.e-10) 
+        x = solver.solve(0.0)
+        self.assertAlmostEqual(x, -1./5.)
+
 if __name__ == "__main__":
     unittest.main()
